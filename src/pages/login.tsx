ce4442d4ceddb213ca96629ce9da7a83c8bac372
SuchithG <suchithgowda1995@gmail.com>
2023-03-27 13:22:49 +0530

    feat: :sparkles: Google Oauth login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 import { useLogin } from "@refinedev/core";
 import { useEffect, useRef } from "react";
 
-import { yariga } from "assets";
+import { yariga } from "../assets";
 
 import { CredentialResponse } from "../interfaces/google";
 
@@ -23,7 +23,7 @@ export const Login: React.FC = () => {
       try {
         window.google.accounts.id.initialize({
           ux_mode: "popup",
-          client_id: GOOGLE_CLIENT_ID,
+          client_id: process.env.REACT_APP_GOOGLE_APP_ID,
           callback: async (res: CredentialResponse) => {
             if (res.credential) {
               login(res);
@@ -52,6 +52,9 @@ export const Login: React.FC = () => {
         alignItems: "center",
       }}
     >
+      <div>
+        <img src={yariga} alt="Yariga Logo"/>
+      </div>
       <GoogleButton />
     </div>
   );
